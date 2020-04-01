3701cb11c427d7df8a767f3b140955c5f7f55279
cmorgoth <cristian.pena@caltech.edu>
2020-04-01 15:51:04 -0700

    semi-working version;need more work on the color squeme

--- scripts/python/make_density_matrix.py
+++ scripts/python/make_density_matrix.py
@@ -21,9 +21,20 @@ zmin = 0.0
 #levels = np.arange(zmin, zmax, 0.4)
 #print(levels)
 # Boost the upper limit to avoid truncation errors.
-norm = cm.colors.Normalize(vmax=zmax, vmin=zmin)
-cmap = cm.PRGn
+n_steps = 250
+#norm = cm.colors.Normalize(vmax=zmax, vmin=zmin)
+#cmap = cm.get_cmap('viridis', n_steps)
+#cmap = cm.get_cmap('RdBu',n_steps)
+cmap = cm.get_cmap('coolwarm',n_steps)
 
+top = cm.get_cmap('Oranges_r', 125)
+bottom = cm.get_cmap('Blues', 125)
+
+newcolors = np.vstack((top(np.linspace(0, 1, 125)),bottom(np.linspace(0, 1, 125))))
+
+print(newcolors)
+
+#print(len(cmap))
 #print( cm.get_cmap(cmap, len(levels) - 1))
 
 #my_mpa = cm.get_cmap(cmap, len(levels) - 1)
@@ -33,29 +44,71 @@ cmap = cm.PRGn
 fig = plt.figure()
 ax = fig.add_subplot(111, projection = "3d")
 
-ax.set_xlabel("x")
-ax.set_ylabel("y")
-ax.set_zlabel("z")
-ax.set_xlim3d(0,10)
-ax.set_ylim3d(0,10)
+##############################
+#####define x-axis labels#####
+##############################
+labels_x = ['', r'$| e \rangle$','', r'$| \ell \rangle$']
+labels_y = ['', r'$\langle e |$','', r'$\langle \ell |$']
 
-xpos = [2,5,8,2,5,8,2,5,8]
-ypos = [1,1,1,5,5,5,9,9,9]
-zpos = np.zeros(9)
+ax.set_xlabel('')
+ax.set_ylabel('')
+ax.set_zlabel("Probability")
+ax.set_xlim3d(-1.0,1.0)
+ax.set_ylim3d(-1.0,1.0)
+ax.set_zlim3d(-1.0,1.0)
 
-dx = np.ones(9)
-dy = np.ones(9)
-dz = [np.random.random(9) for i in range(200)]  # the heights of the 4 bar sets
+ax.set_xticklabels(labels_x)
+ax.tick_params(axis='x', which='major', labelsize=20)
 
-print(dz)
+ax.set_yticklabels(labels_y)
+ax.tick_params(axis='y', which='major', labelsize=20)
+
+#ax.set_zlim3d(0.0,1.0)
+
+xpos = [-0.75, 0.25]
+ypos = [-0.75, 0.25]
+
+xpos, ypos = np.meshgrid(xpos, ypos)
+
+xpos = xpos.flatten()
+ypos = ypos.flatten()
+print(xpos)
+print(ypos)
+zpos = np.zeros_like(xpos)
+
+#dx = np.ones_like(xpos)
+#dy = np.ones_like(ypos)
+dx = np.empty(4)
+dx.fill(0.5)
+dy = np.empty(4)
+dy.fill(0.5)
+print(dy,dx)
+#dz = np.linspace(0.0, 1.0, num=100)
+dz = []
+#n_steps = 100
+step = 1.0/n_steps
+bin1 = 0.95
+bin2 = 0.05
+bin3 = 0.25
+bin4 = -0.55
+for i in range(n_steps):
+    #print(i)
+    #print (np.array([bin1*float(i)*step,bin1*float(i)*step,bin1*float(i)*step,bin1*float(i)*step]))
+    dz.append(np.array([step*bin1,step*bin2,step*bin3,step*bin4]))
+
+#print(dz)
 #print(cm.colors.to_rgb(0.1))
+
+#print(cmap(0))
+#print(dz[0])
 
 _zpos = zpos   # the starting zpos for each bar
 colors = ['r', 'b', 'g', 'y']
-for i in range(200):
+for i in range(n_steps):
     # (colors[i])
-    ax.bar3d(xpos, ypos, _zpos, dx, dy, dz[i], color=cmap(i))
+    ax.bar3d(xpos, ypos, _zpos, dx, dy, dz[i], color=(cmap(0.5-i*step*bin1/2.0),cmap(0.5-i*step*bin2/2.0),cmap(0.5-i*step*bin3/2.0),cmap(0.5-i*step*bin4/2.0)))
     _zpos += dz[i]    # add the height of each bar to know where to start the next
+    #print(_zpos)
 
 plt.gca().invert_xaxis()
 plt.show()
